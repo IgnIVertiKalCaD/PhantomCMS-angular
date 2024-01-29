1d1ab16d567b6c062ada74837a917aebc67ad404
IgnIVertiKalCaD <idrobot696@gmail.com>
2024-01-30 01:19:13 +0300

    fix lagging navigation router (not sure)

---
+++ src/app/animations/fadeScale.animation.ts
@@ -0,0 +1,34 @@
+import {animate, animateChild, group, query, style, transition, trigger} from "@angular/animations";
+
+export const fadeScaleAnimation =
+  trigger('modalOpenAnimation', [
+    transition(':enter', [
+      style({opacity: 0}),
+      animate('150ms cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1})),
+    ]),
+    transition(':leave', [
+      query('section', [
+        animate('150ms cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'scale(0.85)', opacity: 0})),
+      ]),
+
+      animate('100ms', style({opacity: 0})),
+    ])
+  ])
+
+//for development
+
+
+// export const fadeScaleAnimation =
+//   trigger('modalOpenAnimation', [
+//     transition(':enter', [
+//       style({opacity: 0}),
+//       animate('1s cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1})),
+//     ]),
+//     transition(':leave', [
+//       query('section', [
+//         animate('1s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'scale(0.85)', opacity: 0})),
+//       ]),
+//
+//       animate('1s', style({opacity: 0})),
+//     ])
+//   ])
